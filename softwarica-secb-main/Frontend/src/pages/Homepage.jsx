b668059831f82876e5de489c8ebe50617fc76573
dhawit <tdhawit@gmail.com>
2024-08-11 12:19:36 +0545

    Create Homepage.jsx

---
+++ softwarica-secb-main/Frontend/src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Products from './Products'
+
+
+const Homepage = () => {
+  return (
+    <div>
+      <div style={{ marginTop: '30px' }}> 
+        <Products />
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
